ba90c0077cd42412e684eaabd8de717a1e09c2de
BiniyamGeremew <biniyamgeremew42@gmail.com>
2026-02-03 21:42:46 -1200

    Design register page layout

--- accounts/registration_form.py
+++ accounts/registration_form.py
@@ -5,11 +5,36 @@ from captcha.fields import CaptchaField  # simple captcha
 
 
 class RegisterForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=True, label="First Name")
-    father_name = forms.CharField(max_length=30, required=True, label="Father Name")
-    grand_father_name = forms.CharField(max_length=30, required=True, label="Grand Father Name")
-    email = forms.EmailField(required=True, label="Email")
-    captcha = CaptchaField(label="Verification Code")
+    first_name = forms.CharField(
+        max_length=30,
+        required=True,
+        label="First Name",
+        widget=forms.TextInput(attrs={'placeholder': 'Your Name'})
+    )
+    father_name = forms.CharField(
+        max_length=30,
+        required=True,
+        label="Father Name",
+        widget=forms.TextInput(attrs={'placeholder': 'Your Father Name'})
+    )
+    grand_father_name = forms.CharField(
+        max_length=30,
+        required=True,
+        label="Grand Father Name",
+        widget=forms.TextInput(attrs={'placeholder': 'Your Grand Father Name'})
+    )
+    email = forms.EmailField(
+        required=True,
+        label="Email",
+        widget=forms.EmailInput(attrs={'placeholder': 'Your Email Address'})
+    )
+    phone_number = forms.CharField(
+        max_length=15,
+        required=False,
+        label="Phone Number",
+        widget=forms.TextInput(attrs={'placeholder': 'Your Phone Number'})
+    )
+    captcha = CaptchaField(label="CAPTCHA") 
 
     class Meta:
         model = User
@@ -18,6 +43,7 @@ class RegisterForm(UserCreationForm):
             'father_name',
             'grand_father_name',
             'email',
+            'phone_number',
             'username',
             'password1',
             'password2',
